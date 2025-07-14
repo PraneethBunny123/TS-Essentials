d177e42e26449dc14a85cff00e946ead8f1b8dd3
praneeth <gp.gunturu@gmail.com>
2025-07-14 17:50:56 -0500

    If we want to assign specific number of values inside the array we can choose tuples

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -4,3 +4,13 @@ let hobbies = ['cook', 'dance']
 
 // let users: (string | number)[]
 let users = Array<string | number>;
+
+let results: number[] //[1,0]
+
+results = [1,2,3]
+
+// tuples
+let res: [number, number];
+
+res = [1,2];
+// res = [1,2,3]; cause 
