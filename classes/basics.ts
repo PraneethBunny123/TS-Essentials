2f3e461e08e53e066ca3488827791673b18d7d93
praneeth <gp.gunturu@gmail.com>
2025-07-20 20:40:29 -0500

    Since it is a bit longer ts made it easier: We can assign the type inside the constructor as arguments by using  specila keywords like public. Now we can create the object using this class as in same as js

--- classes/basics.ts
+++ classes/basics.ts
@@ -1,9 +1,15 @@
-class User {
-    name: string;
-    age: number
+// class User {
+//     name: string;
+//     age: number
 
-    constructor(n: string, a:number) {
-        this.name = n;
-        this.age = a;
-    }
+//     constructor(n: string, a:number) {
+//         this.name = n;
+//         this.age = a;
+//     }
+// }
+
+class User{
+    constructor(public name: string, public age: number) {}
 }
+
+const max = new User('max', 38)
