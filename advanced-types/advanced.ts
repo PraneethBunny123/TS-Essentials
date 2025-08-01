739012b3bf1fcd06bb6e91ea7085fbb7533fba3f
praneeth <gp.gunturu@gmail.com>
2025-08-01 18:50:10 -0500

    "as const" is a typescript feature makes the roles as readOnly by narrowing the type

--- advanced-types/advanced.ts
+++ advanced-types/advanced.ts
@@ -8,3 +8,7 @@ store.id = 3
 store.isOpen = true
 
 console.log(typeof(store.id))
+
+// as const is a typescript feature makes the roles as readOnly by narrowing the type
+
+let roles = ['admin', 'user', 'editor'] as const
