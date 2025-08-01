6524280f5e92331f56a1941226bc18fd9e047e8a
praneeth <gp.gunturu@gmail.com>
2025-08-01 16:43:19 -0500

    function overloads. Here for the previous problem, we can just add function overloads as you can see in line 1,2 as set the return types of the functions

--- advanced-types/functions.ts
+++ advanced-types/functions.ts
@@ -1,3 +1,6 @@
+function getLength(val: string): string;
+function getLength(val: any[]): number;
+
 function getLength(val: string | any[]) {
     if(typeof(val) === 'string') {
         const numWords = val.split(' ').length // returns the number of words in the sentence 
