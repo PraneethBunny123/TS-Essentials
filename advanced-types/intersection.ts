70385e64d39b7cb46bed9631995bdb7030da162f
praneeth <gp.gunturu@gmail.com>
2025-08-01 15:56:58 -0500

    type guard: since the type will of object with either path or connectionurl we can check it with a simple if loop

--- advanced-types/intersection.ts
+++ advanced-types/intersection.ts
@@ -1,39 +1,39 @@
-// type FileData = {
-//     path: string,
-//     content: string
-// }
+type FileData = {
+    path: string,
+    content: string
+}
 
-// type DatabaseData = {
-//     connectUrl: string,
-//     credentials: string
-// }
+type DatabaseData = {
+    connectUrl: string,
+    credentials: string
+}
 
-// type Status = {
-//     isOpen: boolean,
-//     errorMessage: string
-// }
+type Status = {
+    isOpen: boolean,
+    errorMessage: string
+}
 
-// type AccessedFileData = FileData & Status
-// type AccessedDatabaseData = DatabaseData & Status
+type AccessedFileData = FileData & Status
+type AccessedDatabaseData = DatabaseData & Status
 
 
 //////////////////////////////////
 // Interface
 
-interface FileData {
-    path: string,
-    content: string
-}
+// interface FileData {
+//     path: string,
+//     content: string
+// }
 
-interface DatabaseData {
-    connectUrl: string,
-    credentials: string
-}
+// interface DatabaseData {
+//     connectUrl: string,
+//     credentials: string
+// }
 
-interface Status {
-    isOpen: boolean,
-    errorMessage: string
-}
+// interface Status {
+//     isOpen: boolean,
+//     errorMessage: string
+// }
 
-interface AccessedFileData extends FileData, Status {}
-interface AccessedDatabaseData extends DatabaseData, Status {}
+// interface AccessedFileData extends FileData, Status {}
+// interface AccessedDatabaseData extends DatabaseData, Status {}
