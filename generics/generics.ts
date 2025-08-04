6f4ce3194ccdaf6fba9d131600be96bd1c4f0162
praneeth <gp.gunturu@gmail.com>
2025-08-03 19:36:22 -0500

    multiple place holders inside generic types

--- generics/generics.ts
+++ generics/generics.ts
@@ -15,8 +15,14 @@ store.isLearning = true
 
 ////////////////
 
-function merge<T>(a: T, b: T) {
+// function merge<T>(a: T, b: T) {
+//     return [a, b]
+// }
+
+// const ids = merge<number>(1, 2) // merge(1, 2) also works
+
+function merge<T, U>(a: T, b: U) {
     return [a, b]
 }
 
-const ids = merge<number>(1, 2) // merge(1, 2) also works
+const ids = merge<number, string>(1, "bunny") // merge(1, "bunny") also works
